c28f3c1cbc93667cb1a9a090874f3b5c4f994569
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-13 11:32:05 -0300

    Criado o arquivo ex009.html
    Criado o arquivo referente ao exercicio da aula 011 professor guanabara.

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 59
+
+console.log(`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60) { //Condição simples.
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,19 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){
+    console.log('Você é Brasileiro')
+}
+else{
+    console.log('Você é Estrangeiro!')
+}
+
+/*
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if(país != 'Brasil'){
+    console.log('Você é Estrangeiro')
+}
+else{
+    console.log('Você é Brasileiro!')
+}
+*/
