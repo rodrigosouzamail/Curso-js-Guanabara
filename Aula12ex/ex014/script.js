dc4ca0b3d4117a0540886523ba104dd83333dba8
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-15 16:09:40 -0300

    primeiro exercício resolvido.
    primeiro exercício resolvido

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora <12) {
+    //Bom dia!
+    img.src = '_imagens/cafe-manha.png'
+    document.body.style.background = '#883408'
+}else if (hora >= 12 && hora <18){
+    //Boa tarde
+    img.src = '_imagens/fimDeTarde.png'
+    document.body.style.background = '#fda550'
+}else{
+    //Boa noite
+    img.src = '_imagens/noite.png'
+    document.body.style.background = '#e51338'
+}
+}
