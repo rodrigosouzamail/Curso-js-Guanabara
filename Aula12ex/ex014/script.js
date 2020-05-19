537a34ff5d1cbd25101d5b6323dbf40a87431dfc
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-19 13:40:29 -0300

    Projeto finalizado
    Bom dia, boa tarde, boa noite.
    
    Com imagens e formatações.

--- Aula12ex/ex014/script.js
+++ Aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = '_imagens/amanhecer.png'
+        document.body.style.background = '#e4cdb2'
+}else if (hora >= 12 && hora < 18){
+        //Boa tarde
+        document.body.style.background = '#e27401'
+        img.src = '_imagens/tarde.png'
+}else{
+        img.src = '_imagens/noite.png'
+        document.body.style.background = '#001549'
+    //Boa noite!
+}
+}
