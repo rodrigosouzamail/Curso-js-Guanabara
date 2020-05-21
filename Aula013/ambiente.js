e7e0d6e9c28f43953ada435cfd1bf19443af7305
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-21 09:55:03 -0300

    Aula 013 exercicios finalizados.

---
+++ Aula013/ambiente.js
@@ -0,0 +1,41 @@
+/*
+
+var contador = 1
+while(contador <=500) {
+    console.log(`passo ${contador}`)
+    contador++
+}
+
+
+//Mesmo código de cima
+
+var c = 1
+ while(c <= 500){
+     console.log(`Passo ${c}`)
+     //console.log('Tudo bem ?')
+     c++
+ }
+
+//Mesmo código de cima de forma burra forançando o programa repetir inúmeras vezes.
+
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+
+
+var contador = 1
+do{
+    console.log (`Passo ${contador}`)
+    contador++
+}while (contador <=6)
+
+*/
+var c = 1 
+do{
+    console.log (`Passo ${c}`)
+    c++
+}while (c <= 6)
