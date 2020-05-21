ccec06b40a4093f547870e53a115a13d727c565f
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-21 11:07:37 -0300

    Iniciando a parte 4 de exercicios
    Criado os arquivos  para realizar os exercícios
    
    ex016 e ex017

---
+++ Aula14/ambiente.js
@@ -0,0 +1,32 @@
+/* var c = 1
+
+while (c <= 5){
+    console.log (c)
+    c++
+}
+
+var c = 1
+do {
+    console.log (c)
+    c++
+} while (c <= 5)
+
+
+var c = 1
+while (c <= 5){
+    console.log (c)
+    c++
+}
+*/
+//--------------------------------------------------------
+/*
+for (var c = 1;c <= 5; c++){
+    console.log (c)
+}
+*/
+
+    console.log ('Vai começar!')
+for (var c =1; c <= 5; c++){
+    console.log(c)
+}
+    console.log('Fim')
