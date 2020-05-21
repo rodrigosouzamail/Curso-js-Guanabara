b224c1868fbb4713af1583d5106e945b8aa8b1fc
Rodrigo Souza <rodrigo.souzamail@gmail.com>
2020-05-21 12:45:19 -0300

    Parte 4 e 5 resolvido.
    exercicio da parte 5 resolvido.

--- Aula14/ambiente.js
+++ Aula14/ambiente.js
@@ -26,7 +26,7 @@ for (var c = 1;c <= 5; c++){
 */
 
     console.log ('Vai começar!')
-for (var c =1; c <= 5; c++){
+    for (var c =1; c <= 5; c++){
     console.log(c)
 }
-    console.log('Fim')
+    console.log('Fim')N
--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,1 +1,25 @@
-alert('Olá!')
+function contar(){
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    let res = document.getElementById ('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 || fim.value >= 3000){
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO!] Faltam dados')
+        
+    } else{
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        for(let c = i; c <= f; c+= p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+}
